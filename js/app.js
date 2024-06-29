4db946045c54ab39ffd6fd76aa7cf56bfcbbf2f8
Leonardo Faustino <154287135+Leoofaust@users.noreply.github.com>
2024-06-29 15:56:45 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,28 @@
+function sobreMim() {
+    window.scrollTo({
+        top: window.scrollY + 850,
+        behavior: 'smooth'
+    });
+}
+
+
+function projetos() {
+    window.scrollTo({
+        top: 1700,
+        behavior: 'smooth'
+    });
+}
+
+function objetivos() {
+    window.scrollTo({
+        top: 2200,
+        behavior: 'smooth'
+    });
+}
+
+function contatos() {
+    window.scrollTo({
+        top: 2800,
+        behavior: 'smooth'
+    });
+}
